617bd9d1f382fbaac5e8a47b451acfcaa73a6de9
JorgeJimenez <0251139@up.edu.mx>
2025-06-15 00:44:07 -0600

    values types

--- jscourse/index.js
+++ jscourse/index.js
@@ -1,1 +1,33 @@
 let js = "amazinx";
+
+//Data Types
+
+//there are two types of values
+////1. Primitive values
+//object values
+//
+
+//Primitive Data Types
+
+//Number
+
+let num = 10;
+
+//String
+let str = "Hello World";
+
+//Boolean
+let isTrue = true;
+let isFalse = false;
+
+//Undefined
+let notDefined;
+
+//Null
+
+let emptyValue = null;
+
+//Symbol (es2015)
+//
+////BigInt (es2020)
+//
