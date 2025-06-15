9f2adfe7c29f64383f05a31eb7a60df275153b0a
JorgeJimenez <0251139@up.edu.mx>
2025-06-15 01:07:06 -0600

    comment

--- jscourse/index.js
+++ jscourse/index.js
@@ -1,30 +1,32 @@
-//Data Types
-
-//there are two types of values
-//1. Primitive values
-//object values
+////Data Types
 //
-
-//Primitive Data Types
-
-//Number
-
-let num = 10;
-
-//String
-let str = "Hello World";
-
-//Boolean
-let isTrue = true;
-let isFalse = false;
-
-//Undefined
-let notDefined;
-
-//Null
-
-let emptyValue = null;
-
-//Symbol (es2015)
+////there are two types of values
+////1. Primitive values
+////object values
+////
 //
-////BigI
+////Primitive Data Types
+//
+////Number
+//
+//let num = 10;
+//
+////String
+//let str = "Hello World";
+//
+////Boolean
+//let isTrue = true;
+//let isFalse = false;
+////
+//////Undefined
+////let notDefined;
+////
+//////Null
+//
+//let emptyValue = null;
+//
+////Symbol (es2015)
+////
+//////BigI
+//
+//
